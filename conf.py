8e16485a2f09ea72e1eee068896be429f514c033
JorgeDeLosSantos <delossantosmfq@gmail.com>
2020-11-12 00:45:20 -0600

    Nikola auto commit.
    Nikola version: 8.0.0

--- conf.py
+++ conf.py
@@ -1019,7 +1019,6 @@ PRETTY_URLS = True
 EXTRA_HEAD_DATA = """
 <meta charset="UTF-8">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 """
 
 # Do you want to customize the nbconversion of your IPython notebook?
